d12e0c0db2983451fda51eafb3cfbdf0735d516b
qidizi <qidizi@qq.com>
2015-12-23 16:03:37 +0800

    e

--- php-error.php
+++ php-error.php
@@ -7,7 +7,12 @@
 <body>
 <?php
 echo '<pre>';
-$log = __DIR__ .'/../log/nginx-php-fpm.txt';
+$log = '/var/log/php.log';
+
+if (!is_writable($log)) {
+    exit("{$log}日志文件不可写:请使用 chmod a+wr {$log}处理一下;");
+}
+
 echo file_get_contents($log);
 file_put_contents($log, '');
 echo '</pre>';
