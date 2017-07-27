2be7b7c2eee2780bb7b076fec5c612aa4044b27b
qidizi <qidizi@qq.com>
2017-07-27 14:04:37 +0800

    优化

--- eval.php
+++ eval.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<?php require_once __DIR__ .'/error.php';?><!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -95,37 +95,6 @@ function showLineNO(line){//显示行号原则是只增不减
 </script>
 
 <?php
-
-function throwError($errno=0, $errstr='', $errfile='', $errline=0){
-    echo <<<er
-<fieldset><legend>运行时出错</legend><pre>
-文件:{$errfile}
-行号:{$errline}
-出错代号:{$errno}
-错误:{$errstr}
-</pre></fieldset>
-er;
-}
-
-function throwError2(){
-        $e = error_get_last();
-
-        if (empty($e))
-                return;
-
-    echo <<<er
-<fieldset><legend>致命错误</legend><pre>
-文件:{$e['file']}
-行号:{$e['line']}
-出错代号:{$e['type']}
-错误:{$e['message']}
-</pre></fieldset>
-er;
-}
-set_error_handler("throwError", E_ALL );
-set_exception_handler("throwError");
-register_shutdown_function('throwError2');//如果使用了exit将不运行此脚本
-
 echo '<pre>';
 
 if (!empty($_POST['php'])) {
